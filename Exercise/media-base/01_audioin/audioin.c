86c5821e9e305a104bb207cea8417729ad03c845
gloryd <dzh915@163.com>
2022-06-19 21:35:05 +0800

    media-base:ffmpeglib修改为动态链接方式

--- Exercise/media-base/01_audioin/audioin.c
+++ Exercise/media-base/01_audioin/audioin.c
@@ -53,6 +53,42 @@
 * ---grep -wrns "XvQueryAdaptors" /usr/ -> Binary file /usr/lib64/libva-x11.so.1.4000.0 matches
 * ---grep -wrns "vaGetDisplayDRM" /usr/ -> Binary file /usr/lib64/libva-drm.so.1.4000.0 matches
 * ---grep -wrns "x264_encoder_reconfig" /usr/ -> Binary file /usr/lib64/libx264.so.142 matches
+*
+*
+* 20220619
+* 动态链接ffmpeglib方式可以从根本上解决上述各种依赖问题
+    gloryd@ubuntu20:01_audioin$ ldd 01_audioin
+        linux-vdso.so.1 (0x00007ffd7fb17000)
+        libavcodec.so.58 => /usr/local/ffmpeg/lib/libavcodec.so.58 (0x00007f8092b10000)
+        libavdevice.so.58 => /usr/local/ffmpeg/lib/libavdevice.so.58 (0x00007f8092af1000)
+        libavformat.so.58 => /usr/local/ffmpeg/lib/libavformat.so.58 (0x00007f8092807000)
+        libavutil.so.56 => /usr/local/ffmpeg/lib/libavutil.so.56 (0x00007f8092773000)
+        libc.so.6 => /lib/x86_64-linux-gnu/libc.so.6 (0x00007f809256f000)
+        libswresample.so.3 => /usr/local/ffmpeg/lib/libswresample.so.3 (0x00007f809254a000)
+        libm.so.6 => /lib/x86_64-linux-gnu/libm.so.6 (0x00007f80923f9000)
+        libz.so.1 => /lib/x86_64-linux-gnu/libz.so.1 (0x00007f80923dd000)
+        libfdk-aac.so.2 => /usr/local/lib/libfdk-aac.so.2 (0x00007f8092291000)
+        libspeex.so.1 => /usr/local/lib/libspeex.so.1 (0x00007f8092274000)
+        libx264.so.164 => /usr/local/lib/libx264.so.164 (0x00007f8091fca000)
+        libx265.so.179 => /usr/local/lib/libx265.so.179 (0x00007f8091dea000)
+        libpthread.so.0 => /lib/x86_64-linux-gnu/libpthread.so.0 (0x00007f8091dc5000)
+        libavfilter.so.7 => /usr/local/ffmpeg/lib/libavfilter.so.7 (0x00007f8091958000)
+        libxcb.so.1 => /lib/x86_64-linux-gnu/libxcb.so.1 (0x00007f809192e000)
+        libasound.so.2 => /lib/x86_64-linux-gnu/libasound.so.2 (0x00007f8091833000)
+        libSDL2-2.0.so.0 => /usr/local/lib/libSDL2-2.0.so.0 (0x00007f8091679000)
+        libXv.so.1 => /lib/x86_64-linux-gnu/libXv.so.1 (0x00007f8091474000)
+        libX11.so.6 => /lib/x86_64-linux-gnu/libX11.so.6 (0x00007f8091335000)
+        libXext.so.6 => /lib/x86_64-linux-gnu/libXext.so.6 (0x00007f8091320000)
+        /lib64/ld-linux-x86-64.so.2 (0x00007f809442c000)
+        libdl.so.2 => /lib/x86_64-linux-gnu/libdl.so.2 (0x00007f809131a000)
+        libstdc++.so.6 => /lib/x86_64-linux-gnu/libstdc++.so.6 (0x00007f8091138000)
+        libmvec.so.1 => /lib/x86_64-linux-gnu/libmvec.so.1 (0x00007f809110c000)
+        libswscale.so.5 => /usr/local/ffmpeg/lib/libswscale.so.5 (0x00007f8090f7f000)
+        libpostproc.so.55 => /usr/local/ffmpeg/lib/libpostproc.so.55 (0x00007f8090f5e000)
+        libXau.so.6 => /lib/x86_64-linux-gnu/libXau.so.6 (0x00007f8090f58000)
+        libXdmcp.so.6 => /lib/x86_64-linux-gnu/libXdmcp.so.6 (0x00007f8090f50000)
+        libgcc_s.so.1 => /lib/x86_64-linux-gnu/libgcc_s.so.1 (0x00007f8090f35000)
+        libbsd.so.0 => /lib/x86_64-linux-gnu/libbsd.so.0 (0x00007f8090f19000)
 */
 
 int main(int argc, char *argv[])
@@ -69,7 +105,7 @@ int main(int argc, char *argv[])
 
     if (argc != 2)
     {
-        printf("Usage: ./xx + audioDevide(alsa -> hw:0,0?) + packetnum\n");
+        printf("Usage: ./xx + audioDevide(alsa -> hw:0,0?)\n");
         return -1;
     }
 
